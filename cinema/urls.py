71a06c6c45030868bb181f7a0ecf464871921238
Yura Push <youdeimos1@gmail.com>
2025-01-23 17:41:27 +0200

    add url patterns for genres, cinema halls and movie sessions

--- cinema/urls.py
+++ cinema/urls.py
@@ -4,12 +4,19 @@ from rest_framework import routers
 
 from cinema.views import (
     ActorViewSet,
+    GenreViewSet,
+    CinemaHallViewSet,
+    MovieSessionViewSet,
 )
 
 app_name = "cinema"
 
 router = routers.DefaultRouter()
 router.register("actors", ActorViewSet)
+router.register("genres", GenreViewSet)
+router.register("cinema_halls", CinemaHallViewSet)
+router.register("movies_sessions", MovieSessionViewSet)
+
 
 urlpatterns = [
     path("", include(router.urls)),
