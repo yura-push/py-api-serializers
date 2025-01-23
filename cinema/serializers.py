57cbd7123ca291cdf2d3e2a16c53348d58a2876c
Yura Push <youdeimos1@gmail.com>
2025-01-23 17:39:47 +0200

    instantiate serializers for Genre, CInemaHall and MovieSession models

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -13,3 +13,23 @@ class ActorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Actor
         fields = ["id", "first_name", "last_name"]
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = ["id", "name"]
+
+
+class CinemaHallSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = CinemaHall
+        fields = ["id", "name", "rows", "seats_in_row", "capacity"]
+
+
+class MovieSessionSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = MovieSession
+        fields = ["id", "show_time", "movie", "cinema_hall"]
