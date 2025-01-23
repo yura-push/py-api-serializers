8c298363f00e264f79c1d4bb2e4ca3f76ca4e2aa
Yura Push <youdeimos1@gmail.com>
2025-01-23 17:15:10 +0200

    create viewset for actor model

--- cinema/views.py
+++ cinema/views.py
@@ -1,1 +1,17 @@
-# write views here
+from rest_framework import viewsets
+
+from cinema.models import (
+    Actor,
+    CinemaHall,
+    Genre,
+    Movie,
+    MovieSession,
+)
+from cinema.serializers import (
+    ActorSerializer,
+)
+
+
+class ActorViewSet(viewsets.ModelViewSet):
+    queryset = Actor.objects.all()
+    serializer_class = ActorSerializer
