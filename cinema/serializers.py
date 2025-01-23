335d702c46d979c73b1b11bf85c30eb2dae994d0
Yura Push <youdeimos1@gmail.com>
2025-01-23 17:14:28 +0200

    instantiate serializer for actor model

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -1,1 +1,15 @@
-# write serializers here
+from rest_framework import serializers
+from cinema.models import (
+    Actor,
+    CinemaHall,
+    Genre,
+    Movie,
+    MovieSession
+)
+
+
+class ActorSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Actor
+        fields = ["id", "first_name", "last_name"]
