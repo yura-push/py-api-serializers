5bb39d5c441fa632e5e8558bb7546266ea00a857
Yura Push <youdeimos1@gmail.com>
2025-01-23 17:17:22 +0200

    add urlpattern for actor viewset

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,1 +1,16 @@
-# write urls here
+from django.contrib.admin.widgets import url_params_from_lookup_dict
+from django.urls import path, include
+from rest_framework import routers
+
+from cinema.views import (
+    ActorViewSet,
+)
+
+app_name = "cinema"
+
+router = routers.DefaultRouter()
+router.register("actors", ActorViewSet)
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
